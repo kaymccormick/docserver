6f849c64c297777a0e6dcc1bf6ebb68c60eb15ee
Kay <kay@kaymccormick.com>
2019-03-31 13:47:46 +0000

    dev

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@
  */
 var React = require('react');
 var DocsViewer = require('./docs').default;
+import { Parser } from 'docutils-js/lib/parsers/restructuredtext'
+import { Publisher, publishCmdLine } from 'docutils-js/lib/Core'
+import { newDocument } from 'docutils-js/lib/utils'
+
+publishCmdLine({  argv: ['in.rst'],
+  writerName: 'xml',
+		  description: '',
+}, (...args) => {
+})
 
 export default (props) => <div><h1>helo123</h1>
     <DocsViewer {...props}/>
