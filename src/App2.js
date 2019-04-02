23497a5041346009bc22857e5eeeffd3388aebf6
Kay <kay@kaymccormick.com>
2019-04-02 21:25:55 +0000

    enhancements

--- src/App2.js
+++ src/App2.js
@@ -4,18 +4,51 @@ import { newDocument } from 'docutils-js/lib/utils'
 import { StringInput, StringOutput } from 'docutils-js/lib/io'
 import DocsViewer from './docs2';
 import { getComponentForXmlSync } from 'docutils-react/lib/getComponentForXmlSax'
+import Modal from 'react-modal';
+
+const customStyles = {
+    content : {
+	top                   : '50%',
+	left                  : '50%',
+	right                 : 'auto',
+	bottom                : 'auto',
+	marginRight           : '-50%',
+	transform             : 'translate(-50%, -50%)'
+    }
+};
+
+
 export default class extends React.Component {
     constructor(){
 	super();
-	this.state = {input: 'testx',
-		      output: ''
-		     }
+	this.state = {input: '',
+		      inputModifiedTime: Date.now(),
+		      output: '',
+		      enabled: true,
+		      modalIsOpen: false,
+		      error: null,
+		     };
 
+	this.openModal = this.openModal.bind(this);
+	this.afterOpenModal = this.afterOpenModal.bind(this);
+	this.closeModal = this.closeModal.bind(this);
+	
 	this.handleInputChange = this.handleInputChange.bind(this);
+	this.handleSectionClick = this.handleSectionClick.bind(this);
+	
 	this.outputRef = React.createRef();
+
     }
-    handleInputChange(event) {
-	this.setState( { input: event.target.value});
+
+    onTimeout()
+    {
+	if((this.state.publishTime == null || (Date.now() - this.state.publishTime > 250)) && (this.state.inputModifiedTime > this.state.publishTime)) {
+	    this.publish();
+	}
+	setTimeout(this.onTimeout.bind(this), 500);
+    }
+
+    publish() {
 	const args = { readerName: 'standalone',
 		       parserName: 'restructuredtext',
 		       usage: '',
@@ -24,19 +57,80 @@ export default class extends React.Component {
 		       writerName: 'xml' };
 	
 	const { readerName, parserName, writerName } = args;
-	const source = new StringInput({source: event.target.value})
+	if(!this.state.input) {
+	    return
+	}
+	const source = new StringInput({source: this.state.input})
 	const destination = new StringOutput({})
 	const settings = {}
 	const pub = new Publisher({source, destination, settings});
 	pub.setComponents(readerName, parserName, writerName);
-	pub.publish({}, () => {
+	pub.publish({}, (error) => {
+	    if(error) {
+		console.log(error.stack.constructor.name);
+		this.setState({
+		    modalIsOpen: true,
+		    error });
+		return;
+	    }
 	    this.setState({ output: destination.destination,
+			    publishTime: Date.now(),
 			    component: getComponentForXmlSync(destination.destination)
 			  })
 	});
     }
+
+    componentDidMount() {
+	this.timer = setTimeout(this.onTimeout.bind(this), 1500);
+    }
+
+    openModal() {
+	this.setState({modalIsOpen: true});
+    }
+
+    afterOpenModal() {
+	// references are now sync'd and can be accessed.
+	this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+	this.setState({modalIsOpen: false});
+    }
+    handleSectionClick(event) {
+	/* Do something related to inserting code for a 'section' in the source */
+	event.preventDefault();
+    }
+    
+    handleInputChange(event) {
+	this.setState( { input: event.target.value, inputModifiedTime: Date.now() });
+
+    }
     render() {
-	return <div><form><div style={{display: 'grid'}}><div><textarea id="input" onChange={this.handleInputChange} value={this.state.input}/></div><div><textarea ref={this.outputRef} id="output" value={this.state.output}/></div><div>{this.state.component}</div></div></form></div>;
+	return <div>
+	    <Modal
+        isOpen={this.state.modalIsOpen}
+        onAfterOpen={this.afterOpenModal}
+        onRequestClose={this.closeModal}
+        style={customStyles}
+        contentLabel="Error"
+        >
+            <h2 ref={subtitle => this.subtitle = subtitle}>Error</h2>
+	    <p>{(this.state.error ? this.state.error.message : '') || null}</p>
+	    <div>{(this.state.error ? this.state.error.stack.split('\n').map(x => <div>{x}</div>) : null)}</div>
+            <button onClick={this.closeModal}>close</button>
+            </Modal>
+	    <form><div style={{display: 'grid', gridColumnGap: '10px', gridTemplateColumns: '1fr 1fr'}}>
+	    <div>
+	    <div>
+<button onClick={this.handleSectionClick}>Section</button>
+	    <button onClick={(e) => { this.publish(); e.preventDefault(); }}>Publish</button>
+	    </div>
+	    <div><fieldset {...this.state.enabled ? {} : {disabled: 'disabled'}}>
+	    <textarea style={{width: '40em', height: '15em'}} id="input" onChange={this.handleInputChange} value={this.state.input}/>
+	    </fieldset>
+	    </div>
+	    </div>
+	    <div style={{fontFamily: 'monospace', whiteSpace: 'pre'}}>{this.state.output}</div><div>{this.state.component}</div></div></form></div>;
     }
 }
 
