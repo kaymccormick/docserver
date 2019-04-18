22d213b8d2c32db378422c7aa01acd3d8d041acd
Kay <kay@kaymccormick.com>
2019-04-18 23:30:28 +0000

    eslint autofix

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('@babel/polyfill')
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var compression = require('compression');
-var docRouter = require('./routes/doc.js');
-var publishRouter = require('./routes/publish.js');
+require('@babel/polyfill');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require('compression');
+const docRouter = require('./routes/doc.js');
+const publishRouter = require('./routes/publish.js');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,17 +21,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'static')));
 app.use('/doc-html', express.static(path.join(__dirname, 'doc-html')));
-app.use(compression({ filter: (req, res) => true }))
+app.use(compression({ filter: (req, res) => true }));
 app.use(docRouter);
 app.use(publishRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
