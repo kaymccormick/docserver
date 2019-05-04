5117c22f74ad6b8ddd60423d663187694b2acb23
Kay McCormick <kay@kaymccormick.com>
2019-05-04 16:35:50 -0700

    cant seem to share patch

--- src/Editor.js
+++ src/Editor.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ContentState, Editor, EditorState, convertFromHTML} from 'draft-js';
+import {Editor, EditorState, ContentState, Modifier, convertFromHTML, RichUtils} from 'draft-js';
 
-const blocksFromHTML = convertFromHTML(`test`);
+const blocksFromHTML = convertFromHTML('test');
 
 const state = ContentState.createFromBlockArray(
   blocksFromHTML.contentBlocks,
@@ -14,13 +14,47 @@ const state = ContentState.createFromBlockArray(
 export default class MyEditor extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {editorState: EditorState.createWithContent(state)};
-    this.onChange = (editorState) => this.setState({editorState});
+    this.state = {editorState: EditorState.createEmpty()};
+      this.onChange = (editorState) => this.setState({editorState});
+      this.handleSectionClick = this.handleSectionClick.bind(this);
+      this.handleDebugbClick = this.handleDebugClick.bind(this);
   }
+
+    handleDebugClick() {
+	const contentState = this.state.editorState.getCurrentContent();
+	const blockMap = contentState.getBlockMap();
+    }
+    handleSectionClick() {
+	const contentState = this.state.editorState.getCurrentContent();
+	const contentStateWithEntity = contentState.createEntity('SECTION', 'MUTABLE')
+
+	const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+	const contentStateWithLink = Modifier.applyEntity(
+	    contentStateWithEntity,
+	    selectionState,
+	    entityKey
+	);
+	const newEditorState = EditorState.push(editorState, { currentContent: contentStateWithLink });
+    }
+    handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+    }
+    
   render() {
       return (
-	      <div style={{ borderStyle: '1px solid black'}}>
+	      <div>
+	      <div>
+	      <button onClick={this.handleSectionClick}>Section</button>
+	      <button onClick={this.handleDebugClick}>Debug</button>
+	      </div>
+	      <div style={{ padding: '.66rem', height: '30rem', border: '3px inset gray'}}>
               <Editor editorState={this.state.editorState} onChange={this.onChange} />
+	      </div>
 	      </div>
     );
   }
---
+++ src/mapping.js
@@ -0,0 +1,9 @@
+exports.default = {
+    editor: {
+	path: '/editor',
+	entry: '/editorbundle.js',
+	view: 'editor',
+    }
+};
+
+	
